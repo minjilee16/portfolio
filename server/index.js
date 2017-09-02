844d63352903870c14c2448c28286111dd44f6da
Minji Lee <minjilee8816@gmail.com>
2017-09-02 11:37:55 -0700

    move node modules to gitignore

--- server/index.js
+++ server/index.js
@@ -1,29 +1,25 @@
+
 var express = require('express');
 var bodyParser = require('body-parser');
-// UNCOMMENT THE DATABASE YOU'D LIKE TO USE
-// var items = require('../database-mysql');
-// var items = require('../database-mongo');
 
 var app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+// parse application/json 
+app.use(bodyParser.json())
+// var connection = require('../database-mysql'); 
 
 // UNCOMMENT FOR REACT
 app.use(express.static(__dirname + '/../react-client/dist'));
 
-// UNCOMMENT FOR ANGULAR
-// app.use(express.static(__dirname + '/../angular-client'));
-// app.use(express.static(__dirname + '/../node_modules'));
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
+
+
+
+app.listen(8000, function() {
+  console.log('listening on port 8000!');
 });
 
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
-});
-
+ 
