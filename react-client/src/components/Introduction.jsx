30425d1f214600f326aad97083e73cd9befc1414
Minji Lee <minjilee8816@gmail.com>
2017-09-02 20:14:33 -0700

    add my profile info into introduction component

--- react-client/src/components/Introduction.jsx
+++ react-client/src/components/Introduction.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
 const Introduction = () => (
-  <section id="about" className="content-section">
-    <div className="col-lg-4">
-      <h2>About Minji</h2>
-      <p>Minji is a passionate ui ux designer</p>       
-    </div>
-    <div className="col-lg-4">
-      helelo
+  <section id="about" className="content-section intro">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 mx-auto">
+          <h2 className="about-me">About ME</h2>
+          <p className="intro-paragraph">I'm a passionate UI, UX designer and Front-End developer with experience in developing the web and mobile applications in a team environment. Trained as a full-stack engineer with more focus on front-end development with a good sense of UI, UX design. Also, I have a positive attitude in deadline-oriented environments, arranging plans, ability to prioritize and multi-task in a challenging fast paced environment, and capability to work well in teams. I have experience with the following technology stacks: 
+          </p>
+        </div> 
+        <div className="col-lg-12 mx-auto">
+          <p className="intro-paragraph"><b className="intro-sub">Experienced:</b> JavaScript(ES6), React, React Routers, Redux, Socket I/O, Node, Express, MongoDB, MySQL, PostgreSQL, SQLite, jQuery, HTML, CSS, Git/Github, Heroku, TDD, Agile Development, Adobe Photoshop, Adobe Illustrator, Adobe InDesign, Adobe Experience, Adobe Flash, Adobe Premiere, Bootstrap</p>
+          <p className="intro-paragraph"><b className="intro-sub">Familiar:</b> Angular, D3, Backbone, Mocha, Chai, Jasmine, Adobe Dreamweaver, SketchUp, 3D Rhino, 3D Max, Auto CAD</p>      
+        </div> 
+      
+      </div>
     </div>
   </section>
 )
