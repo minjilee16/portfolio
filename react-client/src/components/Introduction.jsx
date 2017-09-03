923031737408df972b19b57af88d28ec21ce7db4
Minji Lee <minjilee8816@gmail.com>
2017-09-03 12:57:29 -0700

    added education info

--- react-client/src/components/Introduction.jsx
+++ react-client/src/components/Introduction.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Col from 'react-bootstrap/lib/Col';
 
 const Introduction = () => (
   <section id="about" className="content-section intro">
@@ -11,8 +12,25 @@ const Introduction = () => (
         </div> 
         <div className="col-lg-12 mx-auto">
           <p className="intro-paragraph"><b className="intro-sub">Experienced:</b> JavaScript(ES6), React, React Routers, Redux, Socket I/O, Node, Express, MongoDB, MySQL, PostgreSQL, SQLite, jQuery, HTML, CSS, Git/Github, Heroku, TDD, Agile Development, Adobe Photoshop, Adobe Illustrator, Adobe InDesign, Adobe Experience, Adobe Flash, Adobe Premiere, Bootstrap</p>
-          <p className="intro-paragraph"><b className="intro-sub">Familiar:</b> Angular, D3, Backbone, Mocha, Chai, Jasmine, Adobe Dreamweaver, SketchUp, 3D Rhino, 3D Max, Auto CAD</p>      
+          <p className="intro-paragraph"><b className="intro-sub">Familiar:</b> Angular, D3, Backbone, Mocha, Chai, Jasmine, Adobe Dreamweaver, SketchUp, 3D Rhino, 3D Max, Auto CAD</p>
         </div> 
+       
+        <Col className="education bachelor" xs={2} md={4} >
+          <h4 className="school-name">Sookmyung <br/>Women's University</h4><br/>
+          <h6 className="school-level">Bachelor's degree</h6>
+          Visual Communication Design<br/>
+          Environmental Design
+        </Col>
+        <Col className="education master" xs={2} md={4} >
+         <h4 className="school-name">Lincoln University</h4><br/>
+         <h6 className="school-level">Master of Business Administration</h6>
+         International Business
+        </Col>
+        <Col className="education hack" xs={2} md={3} >
+          <h4 className="school-name">Hack Reactor</h4><br/>
+          <h6 className="school-level">Advance Software Engineering immersive</h6>
+          Immersive Javascript training for Software Development
+        </Col>
       
       </div>
     </div>
--- react-client/src/components/Work.jsx
+++ react-client/src/components/Work.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
+import Button from 'react-bootstrap/lib/Button';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import Thumbnail from 'react-bootstrap/lib/Thumbnail';
 
 const Work = (props) => (
-  <section id="download" className="download-section content-section text-center">
-    <div className="container">
-      <div className="col-lg-8 mx-auto">
-        <h2>Download Grayscale</h2>
-        <p>You can download Grayscale for free on the preview page at Start Bootstrap.</p>
-        <a href="http://startbootstrap.com/template-overviews/grayscale/" className="btn btn-default btn-lg">Visit Download Page</a>
-      </div>
-    </div>
+  <section id="download" className="work-section content-section text-center">
+  <Grid>
+    <Row>
+    <Col xs={6} md={3}>
+      <Thumbnail href="#" alt="171x180" src="../../dist/img/intro-bg.jpg" />
+    </Col>
+    <Col xs={6} md={3}>
+      <Thumbnail href="#" alt="171x180" src="../../dist/img/intro-bg.jpg" />
+    </Col>
+    <Col xs={6} md={3}>
+      <Thumbnail href="#" alt="171x180" src="../../dist/img/intro-bg.jpg" />
+    </Col>
+    </Row>
+  </Grid>
   </section>
 )
 
 export default Work;
+
+
+
+  // <section id="download" className="download-section content-section text-center">
+  //   <div className="container">
+  //     <div className="col-lg-8 mx-auto">
+  //       <h2>Work</h2>
+  //       <p>You can download Grayscale for free on the preview page at Start Bootstrap.</p>
+  //       <a href="http://startbootstrap.com/template-overviews/grayscale/" className="btn btn-default btn-lg">Visit Download Page</a>
+  //     </div>
+  //   </div>
+  // </section>
