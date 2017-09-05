aaa0aa909a85a15e724447c7f8aeb2d4e01a2955
Minji Lee <minjilee8816@gmail.com>
2017-09-04 23:37:45 -0700

    cleaned up the server code

--- server/index.js
+++ server/index.js
@@ -11,19 +11,6 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/../client'));
 
-
-
-// app.listen(3000 || process.env.PORT,function(){
-//     console.log("up and running on port "+process.env.PORT);
-// }); 
-
 app.listen(process.env.PORT || 8000 ,function(){
-    console.log("up and running on port "+ process.env.PORT);
+    console.log("listening on port "+ process.env.PORT);
 });
-
-
-// app.listen(8000, function() {
-//   console.log('listening on port 8000!');
-// });
-
- 
