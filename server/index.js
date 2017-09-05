bbbacf0066f8e92a254bbeb2d7cf3bf2585bdf5d
Minji Lee <minjilee8816@gmail.com>
2017-09-04 23:30:01 -0700

    changed the server code

--- server/index.js
+++ server/index.js
@@ -12,8 +12,18 @@ app.use(bodyParser.json())
 app.use(express.static(__dirname + '/../client'));
 
 
-app.listen(8000, function() {
-  console.log('listening on port 8000!');
+
+// app.listen(3000 || process.env.PORT,function(){
+//     console.log("up and running on port "+process.env.PORT);
+// }); 
+
+app.listen(process.env.PORT || 8000 ,function(){
+    console.log("up and running on port "+ process.env.PORT);
 });
+
+
+// app.listen(8000, function() {
+//   console.log('listening on port 8000!');
+// });
 
  
